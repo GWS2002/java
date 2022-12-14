a2da4d1a11c818e20ef171b0681756578de79c13
GWS2002 <115549581+GWS2002@users.noreply.github.com>
2022-12-14 00:01:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let name = Guilherme;
+let age = 19;
+let multiply = age * 7;
+let friends = ["Pedro", "Zé António", "Quim Zé"];
+let characteristicsone = [19, "Preguiçoso"];
+let characteristicstwo = [57, "Bêbado"];
+let characteristicsthree = [84, "Alcoólico"];
+let people = [
+    {
+        name: "Pedro",
+        Age: 19
+    },
+    {
+        name: "Zé António",
+        Age: 57
+    },
+    {
+        name: "Quim Zé",
+        Age: 84
+    }
+];
+
+console.log(name, age, people)
